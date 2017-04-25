e98fee290bd2fe32cbc52dfd41cb4f3ef6610e37
alan leonel fernandes maciel <leonel.fernandesmaciel@gmail.com>
2017-04-25 19:33:41 -0300

    agrego controlador y vista de catalogo

---
+++ controllers/catalogo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# try something like
+
+def index():
+    "pagina de inicio del catalogo"
+    return dict(message="hello from catalogo.py")
+
+def caracteristicas ():
+    "pagina caracteristicas de celulares"
+    return dict(message="funcion  caracteristicas")
+
+def comparacion ():
+    "pagina de comparacion entre celulares"
+    return dict(message="funcion  comparacion")
