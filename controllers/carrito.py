0477e3ced8e9cbc02d5b44c82998b79ed60a87ce
alan leonel fernandes maciel <leonel.fernandesmaciel@gmail.com>
2017-04-26 19:59:00 -0300

    agrego controlador y vista del carrito

---
+++ controllers/carrito.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index():
+    "index del carrito"
+    return dict(message="hello from carrito.py")
+
+def detalle():
+    "detalle de la compra"
+    return dict(message="hello from carrito.py")
+
+def confirmacion():
+    "confirmacion de compra"
+    return dict(message="hello from carrito.py")
--- controllers/catalogo.py
+++ controllers/catalogo.py
@@ -5,10 +5,10 @@ def index():
     "pagina de inicio del catalogo"
     return dict(message="hello from catalogo.py")
 
-def caracteristicas ():
+def caracteristicas():
     "pagina caracteristicas de celulares"
     return dict(message="funcion  caracteristicas")
 
-def comparacion ():
+def comparacion():
     "pagina de comparacion entre celulares"
     return dict(message="funcion  comparacion")
