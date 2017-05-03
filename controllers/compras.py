e09ab1e41f7fb9aa02fdc96eb93a60af61a1c9a3
Lucas Noguera <nogueralucasezequiel@gmail.com>
2017-05-03 19:29:27 -0300

    subo controlador y vistas compras

---
+++ controllers/compras.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index():
+    "Opcion para realizar una compra, se detallara un listado a completar"
+    return dict(message="hello from compras.py")
+
+def listado():
+    "Listar las compras que se realizaron, detallado los nombres de proveedor la cantidad y la fecha"
+    return dict(message="hello from compras.py")
+
+def subirComprobante():
+    "Escanear la factura que se realizo la compra"
+    return dict(message="hello from compras.py")
