52f13531dee92d32442ef65ddc24befbef278e49
Lucas Noguera <nogueralucasezequiel@gmail.com>
2017-06-07 19:47:51 -0300

    Envio de e-mail (falta agregar el email al servidor)

--- controllers/carrito.py
+++ controllers/carrito.py
@@ -8,14 +8,16 @@ def index():
 @auth.requires_login()
 def detalle():
     "detalle de la compra"
-    return dict(message="hello from carrito.py")
+    result = mail.send(to=['storephone@gmail.com'],subject='Compra en StorePhone',reply_to='nogueralucasezequiel@gmail.com',message='Muchas Gracias por realizar tu compra en Store Phone, al correr de los dias se les detallara el estado de su compra')
+    return dict(result=result)
+
 
 @auth.requires_login()
 def confirmacion():
     "confirmacion de compra"
-    return dict(message="hello from carrito.py")
+    return dict(message="")
 
 @auth.requires_login()
 def factura():
     "Factura de la compra"
-    return dict(message="hello from carrito.py")
+    return dict(message="")
