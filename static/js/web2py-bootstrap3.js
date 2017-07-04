7529fc09b2407b2ffcf00a4425bfb6459605aa33
Fernandes Maciel Alan <leonel.fernandesmaciel@gmail.com>
2017-07-04 19:28:12 -0300

    commit prueba de servidor

--- static/js/web2py-bootstrap3.js
+++ static/js/web2py-bootstrap3.js
@@ -80,3 +80,16 @@
   });
 
 })(jQuery);
+
+$(document).ready(function() {
+    $('.deliverable-infos').hide();
+    $('.dropdown-deliverable').on('click', function(e) {
+        console.log("dropdown toggled!");
+        e.preventDefault();
+        e.stopPropagation();
+        //get targeted element via data-for attribute
+        var dataFor = $(this).data('for');
+        var idFor = $(dataFor);
+        idFor.slideToggle();
+    }); 
+});
