0a837c2c386ccb55468b2e4282deafeaf78925a1
Lucas Noguera <nogueralucasezequiel@gmail.com>
2017-08-23 19:43:58 -0300

    subo boton mercado pago

---
+++ controllers/pago.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# intente algo como
+def index():
+    return dict(message="hello from pago.py")
+
+def ok:
+    return dict(message="hello from pago.py")
+def pendiente:
+    return dict(message="hello from pago.py")
