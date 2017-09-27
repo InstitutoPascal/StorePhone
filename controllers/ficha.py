591cf69157c74a1ebb1b2e2ad700ce648596991c
Lucas Noguera <nogueralucasezequiel@gmail.com>
2017-09-27 19:39:04 -0300

    arreglos de comparacion y vistas

--- controllers/ficha.py
+++ controllers/ficha.py
@@ -3,7 +3,7 @@
 
 def index():
     "pagina de inicio del catalogo"
-    datos_articulo=db().select(db.articulo.ALL)
+    datos_articulo=db(db.articulo.id==request.args[0]).select()
     return dict (da=datos_articulo)
 
 def caracteristicas():
@@ -11,17 +11,24 @@ def caracteristicas():
     return dict(message="funcion  caracteristicas")
 
 def comparacion():
-    "pagina de comparacion entre celulares"
-    return dict(message="funcion  comparacion")
+    if not session.id_articulo1:
+        #seleccionamos el primer articulo
+        session.id_articulo1=request.args[0]
+        #para que aparezca un mensaje 
+        session.flash="Seleccione el segundo articulo que desea comparar"
+        #redireccionamos hacia el catalogo
+        redirect(URL(c="catalogo", f="index"))
+    else:
+        #seleccionamos el segundo articulo
+        session.id_articulo2=request.args[0]
+    q = db.articulo.id==session.id_articulo1    
+    datos_articulo1 = db(q).select().first()
+    q = db.articulo.id==session.id_articulo2
+    datos_articulo2 = db(q).select().first()
+    return dict(d1=datos_articulo1, d2=datos_articulo2)
 
-def inicio():
-    "pagina de inicio del catalogo"
-    return dict(message="funcion  inicio")
-
-def motorola():
-    "pagina de catalogo discriminados por modelo motorola"
-    return dict(message="funcion  motorola")
-
-def samsung():
-    "pagina de catalogo discriminados por modelo samsung"
-    return dict(message="funcion  samsung")
+def volver():
+    #borramos los articulos seleccionados, para realizar una nueva comparacion
+    del session.id_articulo1
+    del session.id_articulo2
+    redirect(URL(c="catalogo", f="index"))
