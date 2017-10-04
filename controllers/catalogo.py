ecf67f15a8bbd54b3abdf4d069395da43b8d2fc1
Lucas Noguera <nogueralucasezequiel@gmail.com>
2017-10-04 19:05:39 -0300

    elimino controladores innecesarios

--- controllers/catalogo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# try something like
-
-def index():
-    "pagina de inicio del catalogo"
-    datos_articulo=db().select(db.articulo.ALL)
-    return dict (da=datos_articulo)
